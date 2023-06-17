467514b2962cc6c17a98512e26ce272f444c6b96
Adele Yu <fhead17@gmail.com>
2023-06-18 01:11:28 +0800

    chore(config): Update api url and garden title

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
-    'api_url': 'https://api.dabing.one/', // 数据请求地址
-    'title': 'Jiang 的数字花园🌱', // 站点标题
+    'api_url': 'https://404overthinker-hepta-api.vercel.app/', // 数据请求地址
+    'title': 'Tylia\'s Digital Garden', // 站点标题
     'pages': {
-        'Blogs': '9f31ea21-90b9-4523-b8d5-cb33b7a01bda',
-        'Projects': '3dd9a603-a7f3-44e9-a6d7-cd2ebda08952',
-        'Activity':'activity', // 花园活跃状态页面
-        'About': '3a433c0b-e2e1-4722-8a88-a17e9aa2b927'
+        // 'Blogs': '9f31ea21-90b9-4523-b8d5-cb33b7a01bda',
+        // 'Projects': '3dd9a603-a7f3-44e9-a6d7-cd2ebda08952',
+        // 'Activity':'activity', // 花园活跃状态页面
+        'About': 'e90e22b9-7bb2-4bf6-a4d7-81136f8b7064'
     }
 }
 
